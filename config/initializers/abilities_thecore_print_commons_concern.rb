a5c44517e22a835cb68e2b3c0f56700047408a25
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-10-22 23:15:13 +0200

    fix: remove PrintJob reference

--- config/initializers/abilities_thecore_print_commons_concern.rb
+++ config/initializers/abilities_thecore_print_commons_concern.rb
@@ -4,19 +4,18 @@ require 'active_support/concern'
 module ThecorePrintCommonAbilitiesConcern
     extend ActiveSupport::Concern
     included do
-    def thecore_print_commons_abilities user
-        if user
-        # if the user is logged in, it can do certain tasks regardless his role
-        if user.admin?
-            # if the user is an admin, it can do a lot of things, usually
-        end
+        def thecore_print_commons_abilities user
+            if user
+                # if the user is logged in, it can do certain tasks regardless his role
+                if user.admin?
+                    # if the user is an admin, it can do a lot of things, usually
+                end
 
-        if user.has_role? :role
-            # a specific role, brings specific powers
+                if user.has_role? :role
+                    # a specific role, brings specific powers
+                end
+            end
         end
-        cannot [ :create, :update, :destroy, :show, :clone ], PrintJob
-        end
-    end
     end
 end
 
