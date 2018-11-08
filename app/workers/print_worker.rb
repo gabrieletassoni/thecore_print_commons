94ea0fe71086926e63bff592b2188845f4831638
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-11-08 17:10:40 +0100

    feat: add some logging

--- app/workers/print_worker.rb
+++ app/workers/print_worker.rb
@@ -8,9 +8,11 @@ class PrintWorker
         #     printer.close
         # else
         print_file = "/tmp/print-#{printer_cups_name}-#{Time.now.strftime '%Y%m%d%H%M%S%L'}"
+        puts "Creating temp file: #{print_file}"
         IO.write(print_file, text)
         # CupsPrinter.new printer_cups_name, hostname: Settings.ns(:printer_commons).cups_server
         # printer.print_data text, 'text/plain', hostname: Settings.ns(:printer_commons).cups_server
+        puts "Printing with lp command on #{printer_cups_name} of #{Settings.ns(:printer_commons).cups_server} "
         `lp -d "#{printer_cups_name}" -h "#{Settings.ns(:printer_commons).cups_server}" -o raw "#{print_file}"`
         File.delete print_file
         # end
