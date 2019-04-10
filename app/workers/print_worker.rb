00b6708749ef6be86cf6f38ea936bab0324debf4
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2019-04-10 17:41:23 +0200

    add retry false

--- app/workers/print_worker.rb
+++ app/workers/print_worker.rb
@@ -1,6 +1,8 @@
 require 'ipaddr'
 class PrintWorker
     include Sidekiq::Worker
+    sidekiq_options retry: false
+    
     def perform printer_cups_name, text
         # if Settings.ns(:printer_commons).cups_server.blank? || ['127.0.0.1', 'localhost'].contains(Settings.ns(:printer_commons).cups_server)
         #     printer = CupsPrinter.new printer_cups_name
@@ -13,7 +15,7 @@ class PrintWorker
         # CupsPrinter.new printer_cups_name, hostname: Settings.ns(:printer_commons).cups_server
         # printer.print_data text, 'text/plain', hostname: Settings.ns(:printer_commons).cups_server
         puts "Printing with lp command on #{printer_cups_name} of #{Settings.ns(:printer_commons).cups_server} "
-        `lp -d "#{printer_cups_name}" -h "#{Settings.ns(:printer_commons).cups_server}" -o raw "#{print_file}"`
+        `cupsenable "#{printer_cups_name}";lp -d "#{printer_cups_name}" -h "#{Settings.ns(:printer_commons).cups_server}" -o raw "#{print_file}"`
         File.delete print_file
         # end
     end
