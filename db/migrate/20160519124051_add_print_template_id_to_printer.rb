148e5ac868288b6296b9c7d38cb3e9a2c9d7c7d5
Gabriele Tassoni <g.tassoni@bancolini.com>
2023-02-11 17:22:04 +0000

    Split add_reference into add_column and index

--- db/migrate/20160519124051_add_print_template_id_to_printer.rb
+++ db/migrate/20160519124051_add_print_template_id_to_printer.rb
@@ -1,5 +1,6 @@
 class AddPrintTemplateIdToPrinter < ActiveRecord::Migration[7.0]
   def change
-    add_reference :printers, :print_template, index: true, foreign_key: true
+    add_column :printers, :print_template_id, :bigint, if_not_exist: true
+    add_index :printers, :print_template_id, if_not_exist: true
   end
 end
