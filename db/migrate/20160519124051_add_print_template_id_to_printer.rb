0ddc4628540b9fd3fd94ec2914d14443ad93e777
Gabriele Tassoni <g.tassoni@bancolini.com>
2023-02-11 17:44:59 +0000

    Replace if_not_exist with if_not_exists

--- db/migrate/20160519124051_add_print_template_id_to_printer.rb
+++ db/migrate/20160519124051_add_print_template_id_to_printer.rb
@@ -1,6 +1,6 @@
 class AddPrintTemplateIdToPrinter < ActiveRecord::Migration[7.0]
   def change
-    add_column :printers, :print_template_id, :bigint, if_not_exist: true
-    add_index :printers, :print_template_id, if_not_exist: true
+    add_column :printers, :print_template_id, :bigint, if_not_exists: true
+    add_index :printers, :print_template_id, if_not_exists: true
   end
 end
